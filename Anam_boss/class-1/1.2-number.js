9a3953c147836dd902701a26952bf39ab1abd14b
Md. Mahfuz Alam <mahfuz00100@gmail.com>
2019-01-28 19:30:58 +0600

    Number

---
+++ Anam_boss/class-1/1.2-number.js
@@ -0,0 +1,19 @@
+console.log(0b1001); //0b express binary
+console.log(010);   //0 express octal
+console.log(0xf);   //0x express hexadecimal
+console.log(10);    //any number express decimal
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(5e8);
+
+console.log(Number.parseInt("123"));
+console.log(Number.parseFloat("123.34"));
+
+console.log(typeof "123");
+console.log(typeof 123);
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+
+console.log(100.77 + 11.3);
