640aa540448561d3ad68df2290cb0e629d00f7ea
Md. Mahfuz Alam <mahfuz00100@gmail.com>
2019-01-28 19:30:30 +0600

    Delete 1.3-number-project.js

--- Anam_boss/1.3-number-project.js
+++
@@ -1,8 +0,0 @@
-//project of number
-
-//console.log(process.argv); // here are 2 array 0= node.js binary patch and 1=javascript file path
-
-let k = parseInt(process.argv[2]);
-let m = parseInt(process.argv[3]);
-
-console.log(k + m); // to get a result open terminal and write node file-name number number(then enter)
